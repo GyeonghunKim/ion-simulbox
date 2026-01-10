003dc763c67b49db122cafcf7e3b74cbafd5040e
Gyeonghun Kim <gyeonghun.kim@duke.edu>
2026-01-10 10:23:47 -0500

    Remove old

--- ion_toolkit/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ion import Ion
-from .energy_level import EnergyLevel, FineStructure, HyperfineStructure, FineStructureZeemanLevel, HyperfineStructureZeemanLevel
-from .units import Units, Constants
-
-__all__ = ["Ion", "EnergyLevel", "FineStructure", "HyperfineStructure", "FineStructureZeemanLevel", "HyperfineStructureZeemanLevel", "Units", "Constants"]
